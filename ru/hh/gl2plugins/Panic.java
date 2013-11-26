f89a26686f72e2da984758bb8fa1f25a67aa90d3
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-11-26 19:31:28 +0400

    major rewrite

--- ru/hh/gl2plugins/Panic.java
+++ ru/hh/gl2plugins/Panic.java
@@ -9,78 +9,102 @@ import org.graylog2.plugin.streams.Stream;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
+import static java.nio.file.StandardCopyOption.*;
 
 public class Panic implements MessageOutput {
-  public static final String NAME = "Panic";
+    private class LogEntry {
+        Integer level;
+        Integer count;
+        String streams;
 
-  @Override
-  public void initialize(Map<String, String> config) throws MessageOutputConfigurationException {
-  }
-
-  private String escapeHtml(String s) {
-    s = s.replace("<", "&lt;");
-    s = s.replace(">", "&gt;");
-    s = s.replace("&", "&amp;");
-    s = s.replace("\"", "&quot;");
-    return s;
-  }
-
-  @Override
-  public void write(List<LogMessage> messages, OutputStreamConfiguration streamConfiguration, GraylogServer server) throws Exception {
-    if (messages.size() > 0) {
-      Date now = new Date();
-      String filename = (now.getTime() - now.getTime() % 300000) + ".txt";
-      BufferedWriter bw = new BufferedWriter(new FileWriter("/tmp/panic-www/" + filename, true));
-
-      for (LogMessage logMessage : messages) {
-        int streamCount = 0;
-        for (Stream stream : logMessage.getStreams()) {
-          if (stream != null && stream.getTitle() != null && !stream.getTitle().equals("panic")) {
-            streamCount++;
-          }
+        private LogEntry(Integer level, Integer count, String streams) {
+            this.level = level;
+            this.count = count;
+            this.streams = streams;
         }
-        String[] streamArray = new String[streamCount];
-        int i = 0;
-        for (Stream stream : logMessage.getStreams()) {
-          if (stream != null && stream.getTitle() != null && !stream.getTitle().equals("panic")) {
-            streamArray[i++] = stream.getTitle();
-          }
-        }
-        Arrays.sort(streamArray);
-
-        String streams = "<span style='font-weight:bold;color:maroon;'>[";
-        for (String stream : streamArray) {
-          streams += escapeHtml(stream);
-          if (i < streamArray.length - 1) {
-            streams += ",";
-          }
-        }
-        streams += "] </span>";
-        bw.write(streams);
-        bw.write(escapeHtml(logMessage.getFullMessage().replace('\n', ' ')));
-        bw.newLine();
-      }
-      bw.close();
     }
-  }
 
-  @Override
-  public Map<String, String> getRequestedConfiguration() {
-    return new HashMap<String, String>() {{
-      put("stub", "stub");
-    }};
-  }
+    public static final String NAME = "Panic";
 
-  @Override
-  public Map<String, String> getRequestedStreamConfiguration() {
-    return new HashMap<String, String>() {{
-      put("stub", "stub");
-    }};
-  }
+    private static ConcurrentHashMap<String, LogEntry> messageCounts = new ConcurrentHashMap<String, LogEntry>();
 
-  @Override
-  public String getName() {
-    return NAME;
-  }
+    private String getStreamsString(List<Stream> streams) {
+        String res = "";
+        for (Stream s : streams) {
+            if (!(s.getTitle().equals("panic") && streams.size() > 1)) {
+                res += "<a href='http://graylog.hh.ru/streams/" + s.getId() + "-" + s.getTitle() + "/messages'>" + s.getTitle() + "</a> ";
+            }
+        }
+        if (streams.size() == 0) {
+            res = "(no streams)";
+        }
+        return res;
+    }
+
+    @Override
+    public void initialize(Map<String, String> config) throws MessageOutputConfigurationException {
+    }
+
+    @Override
+    public void write(List<LogMessage> messages, OutputStreamConfiguration streamConfiguration, GraylogServer server) throws Exception {
+        if (messages.size() > 0) {
+            for (LogMessage m : messages) {
+                if (m.getLevel() <= 4) {
+                    String escaped = escapeHtml4(m.getShortMessage().replace('\n', ' ')).replaceAll("[0-9]", "");
+                    messageCounts.put(escaped, messageCounts.containsKey(escaped) ?
+                            new LogEntry(messageCounts.get(escaped).level, messageCounts.get(escaped).count + 1, messageCounts.get(escaped).streams)
+                            :
+                            new LogEntry(m.getLevel(), 1, getStreamsString(m.getStreams())));
+                }
+            }
+            long stamp = new Date().getTime() + new Random().nextLong();
+            BufferedWriter bw = new BufferedWriter(new FileWriter("/tmp/panic-www/superpanic.html." + stamp));
+
+            bw.write("<html><head><title>panic</title><link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css\"><link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css\"><script src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js\"></script><meta http-equiv=\"refresh\" content=\"60\"></head><body>");
+            bw.write("<div class='container'><h2><a href='http://graylog.hh.ru'>GRAYLOG</a>&nbsp;&nbsp;&nbsp;" + new Date() + " </h2><table class='table table-striped table-condenced'>");
+
+            LinkedList<Map.Entry<String, LogEntry>> sorted = new LinkedList<Map.Entry<String, LogEntry>>(messageCounts.entrySet());
+            Collections.sort(sorted, new Comparator<Map.Entry<String, LogEntry>>() {
+                @Override
+                public int compare(Map.Entry<String, LogEntry> o1, Map.Entry<String, LogEntry> o2) {
+                    if (!o2.getValue().count.equals(o1.getValue().count)) {
+                        return o2.getValue().count.compareTo(o1.getValue().count);
+                    }
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+            });
+
+            for (Map.Entry<String, LogEntry> e : sorted) {
+                bw.write("<tr" + (e.getValue().level <= 3 ? " class='danger'" : "") + "><td>" + e.getValue().count + "</td><td>" + e.getValue().streams + "</td><td>" + e.getKey() + "</td></tr>\n");
+            }
+            bw.write("</div></table></body></html>");
+            bw.close();
+            Files.move(Paths.get("/tmp/panic-www/superpanic.html." + stamp), Paths.get("/tmp/panic-www/current.html"), ATOMIC_MOVE);
+        }
+    }
+
+    @Override
+    public Map<String, String> getRequestedConfiguration() {
+        return new HashMap<String, String>() {{
+            put("stub", "stub");
+        }};
+    }
+
+    @Override
+    public Map<String, String> getRequestedStreamConfiguration() {
+        return new HashMap<String, String>() {{
+            put("stub", "stub");
+        }};
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
 }
