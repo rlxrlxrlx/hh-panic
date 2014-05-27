2bfa5576ee70a17857163b297521bf694cc74a55
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-05-27 15:46:01 +0400

    add deployment shell script

---
+++ upload_plugin.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ${HOME}/.graylog-plugin-uploader.conf.bash
+
+PANIC_CHECKOUT_DIR=~/idea/hh-panic
+cd ${PANIC_CHECKOUT_DIR}
+mvn package
+scp target/panic-1.0.jar ${SSH_USER}@${RELAY_HOST}:~
+ssh -A ${SSH_USER}@${RELAY_HOST} "scp ~/panic-1.0.jar ${TARGET_HOST}:~/ru.hh.gl2plugins.panic.Panic_gl2plugin.jar"
+ssh -A ${SSH_USER}@${RELAY_HOST} "ssh ${TARGET_HOST} 'sudo cp ~/ru.hh.gl2plugins.panic.Panic_gl2plugin.jar ${TARGET_FOLDER}'"
